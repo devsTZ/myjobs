ae905a4bca6cc94e2eadc72b360f0296ded9bed5
devsTz <rogashianmvungi@gmail.com>
2023-11-26 11:15:43 -0800

    Hide user trial for job seekers

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -4,15 +4,46 @@
 <div class="container mt-5">
     <div class="row justfy-content-center">
         Hello,  {{auth() -> user()-> name}}
-       <p>Your trial will expire on {{auth()->user()->user_trial}}</p>
-    
-       <div class="col-md-3">
-        <div class="card-counter primary">
-            <p class="text-center mt-3 lead">User Profile</p>
-            <button class="btn btn-primary float-end">View</button>
-        </div>
-       </div>
+        @if (Auth::check() && auth()->user()->user_type == 'employer')
+                   <p class="alert alert-warning">Your trial will expire on {{auth()->user()->user_trial}}</p>
+        @endif
+        <div class="row"> 
+  <div class="col-sm-6 col-sm-6 col-md-3 col-lg-3 ">
+    <div class="card bg-primary">
+      <div class="card-body">
+        <h5 class="card-title">User Profile</h5>
+        <a href="#" class="btn btn-dark">View</a>
+      </div>
+    </div>
+  </div>
 
+  <div class="col-sm-6 col-sm-6 col-md-3 col-lg-3 ">
+    <div class="card bg-danger">
+      <div class="card-body">
+        <h5 class="card-title">Post Job</h5>
+        <a href="#" class="btn btn-dark">View</a>
+      </div>
+    </div>
+  </div>
+
+  <div class="col-sm-6 col-sm-6 col-md-3 col-lg-3 ">
+    <div class="card bg-success">
+      <div class="card-body">
+        <h5 class="card-title">All Jobs</h5>
+        <a href="#" class="btn btn-dark">View</a>
+      </div>
+    </div>
+  </div>
+
+  <div class="col-sm-6 col-md-3 col-lg-3 ">
+    <div class="card bg-warning">
+      <div class="card-body">
+        <h5 class="card-title">Item 4</h5>
+        <a href="#" class="btn btn-dark">View</a>
+      </div>
+    </div>
+  </div>
+</div>
     </div>
 </div>
 
